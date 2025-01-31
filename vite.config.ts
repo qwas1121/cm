07b49b996a286a4212fd78d9d9ef196492b37c00
qwas1121 <qwas1121@naver.com>
2025-01-31 17:48:33 +0900

    0131

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 저장소 이름을 정확히 입력해야 함!
+  base: "./", // 저장소 이름을 정확히 입력해야 함!
 });
